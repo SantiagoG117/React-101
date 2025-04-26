ed722d970f5a7089a5a157b106e7cdd611159b92
SantiagoG117 <garc0254@algonquinlive.com>
2025-04-26 13:08:51 -0400

    Create custom React Query hook

---
+++ src/react-query/hooks/useTodos.ts
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+/* 
+React Query has many benefits:
+    1. Replace the variables for the data,loading and errors state, as well as the effectHook
+    2. We get autoretries in case the the call to the server fails
+    3. Automatic refresh: If the data changes while the user is in this page, the page will be re-rendered to reflect the changes
+    4. Caching: The fetched data will be stored in the cache and will be fresh for a certain period of time. 
+        The next time we need the same piece of data. If it is still in the cache we don't have to fetch it from the server
+    Generic types: <Type of data we are fetching, Type of Error>
+*/
+ 
+const useTodos = () => {
+  const fetchTodos = () =>
+    axios
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.data);
+
+  return useQuery<Todo[], Error>({
+    /* Unique identifier for the query. It is interally used for caching. Everytime we retrieve data from the backend, 
+        the data is stored in the cache and can be accessible via the query key 
+        We set the key to an array of one or more values. The values depend on the type of data we want to store but the first
+        value is often a string that identifies the type of data we want to store.
+        */
+    queryKey: ["todos"],
+    /* The function that we use to fetch the data from the backend.*/
+    queryFn: fetchTodos,
+  });
+};
+
+export default useTodos;
